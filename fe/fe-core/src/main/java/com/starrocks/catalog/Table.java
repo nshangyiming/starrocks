5f1e0f9dd4762828900d3b4f073ffcb92e8c83b7
Dejun Xia <xiadejun@starrocks.com>
2023-10-02 11:53:25 -0700

    [BugFix] Fix analyze mv privilege check
    Signed-off-by: Dejun Xia <xiadejun@starrocks.com>

--- fe/fe-core/src/main/java/com/starrocks/catalog/Table.java
+++ fe/fe-core/src/main/java/com/starrocks/catalog/Table.java
@@ -767,4 +767,10 @@ public class Table extends MetaObject implements Writable, GsonPostProcessable {
         }
         return ret;
     }
+
+    public boolean isTable() {
+        return !type.equals(TableType.MATERIALIZED_VIEW) &&
+                !type.equals(TableType.CLOUD_NATIVE_MATERIALIZED_VIEW) &&
+                !type.equals(TableType.VIEW);
+    }
 }
